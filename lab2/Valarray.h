04cfe29317a5bff829275ced4b7bc573f10e5182
Xingyuan Wang <xingyuan.wang.zju@gmail.com>
2015-04-15 09:50:34 -0500

    Final Version

--- lab2/Valarray.h
+++ lab2/Valarray.h
@@ -5,23 +5,21 @@
 //using std::vector; // during development and testing
 using epl::vector; // after submission
 
+template <typename V> struct vec_wrap;
+
 // If operand is a proxy, we should use its value instead of its reference.
 template <typename T> struct to_ref { using type = T; };
-
 template <typename T> struct to_ref<vector<T>> { using type = const vector<T>&; };  // Here const is important!!!
-
 template <typename T> using Ref = typename to_ref<T>::type;   // type alias
 
 // Choose appropriate return type
-template <typename T> struct rank { static constexpr int value = 0; };
+template <typename T> struct rank { static constexpr int value = 0; };  // used in EnableIf
 template <> struct rank<int> { static constexpr int value = 1; };
 template <> struct rank<float> { static constexpr int value = 2; };
 template <> struct rank<double> { static constexpr int value = 3; };
 template <typename T> struct rank<std::complex<T>> { static constexpr int value = rank<T>::value; };
 
-template <int R>
-struct stype;
-
+template <int R> struct stype;
 template <> struct stype<1> { using type = int; };
 template <> struct stype<2> { using type = float; };
 template <> struct stype<3> { using type = double; };
@@ -48,13 +46,18 @@ struct choose_type {
     using type = typename ctype<my_complex, my_type>::type;
 };
 
-template <typename T1, typename T2>
-using get_type = typename choose_type<typename T1::value_type, typename T2::value_type>::type;
+
+// get appropriate value type of T1, T2. T1, T2 can be vec_wrap, primitive types, etc.
+template <typename T1, typename T2> struct get_type;
+template <typename V1, typename V2>
+struct get_type<vec_wrap<V1>, vec_wrap<V2>> { using type = typename choose_type<typename V1::value_type, typename V2::value_type>::type; };
+template <typename V, typename T>
+struct get_type<vec_wrap<V>, T> { using type = typename choose_type<typename V::value_type, T>::type; };
+template <typename T, typename V>
+struct get_type<T, vec_wrap<V>> { using type = typename choose_type<T, typename V::value_type>::type; };
 
 template <bool p, typename T>
 using EnableIf = typename std::enable_if<p, T>::type;
-
-template <typename V> struct vec_wrap;
 
 template <typename T>
 using valarray = vec_wrap<vector<T>>;
@@ -109,7 +112,7 @@ public:
     Ref<Right> _r;
     FUN _op;
 public:
-    using value_type = get_type<Left, Right>;
+    using value_type = typename choose_type<typename Left::value_type, typename Right::value_type>::type;;
     BinaryProxy() = default;
     BinaryProxy(Ref<Left> l, Ref<Right> r, FUN op): _l(l), _r(r), _op(op) {}
     BinaryProxy(const BinaryProxy& that) : _l(that._l), _r(that._r), _op(that._op) {}
@@ -174,7 +177,7 @@ struct vec_wrap : public V {
 
     ~vec_wrap() = default;
     
-    vec_wrap& operator=(const vec_wrap<V>& that) {
+    vec_wrap& operator=(const vec_wrap<V>& that) {  // This is for case x = y, both x and y are valarray<int>
         if ((void*)this != (void*)&that) {
             uint64_t min_size = this->size() < that.size() ? this->size() : that.size();
             for (uint64_t i = 0; i < min_size; ++i) {
@@ -185,7 +188,7 @@ struct vec_wrap : public V {
     }
     
     template <typename RHS>
-    vec_wrap& operator=(const vec_wrap<RHS>& that) {
+    vec_wrap& operator=(const vec_wrap<RHS>& that) {    // This is for case x = y, y is any type of vec_wrap but x's
         if ((void*)this != (void*)&that) {
             uint64_t min_size = this->size() < that.size() ? this->size() : that.size();
             for (uint64_t i = 0; i < min_size; ++i) {
@@ -193,6 +196,11 @@ struct vec_wrap : public V {
             }
         }
         return *this;
+    }
+    
+    template <typename T>
+    EnableIf<rank<T>::value, void> operator=(const T& that) {   // Use EnableIf in case of meaningless x = foo
+        this->operator=(vec_wrap<Scalar<T>>(that));
     }
     
     template <typename FUN>
@@ -205,11 +213,6 @@ struct vec_wrap : public V {
     
     typename std::plus<typename V::value_type>::result_type sum() {
         return accumulate(std::plus<typename V::value_type>{});
-    }
-    
-    template <typename T>
-    EnableIf<rank<T>::value, void> operator=(const T& that) {
-        this->operator=(vec_wrap<Scalar<T>>(that));
     }
     
     template <typename FUN>
@@ -226,71 +229,43 @@ struct vec_wrap : public V {
     }
 };
 
-/******************************************/
-// binary operations
-/******************************************/
-// Add
-template <typename V1, typename V2, typename FUN = std::plus<get_type<V1, V2>>>
-vec_wrap<BinaryProxy<V1, V2, FUN>> operator+(const vec_wrap<V1>& lhs, const vec_wrap<V2>& rhs) {
-    return vec_wrap<BinaryProxy<V1, V2, FUN>>(BinaryProxy<V1, V2, FUN>(lhs, rhs, FUN{}));
-}
-template <typename T, typename V, typename FUN = std::plus<get_type<Scalar<T>, V>>>
-EnableIf<rank<T>::value, vec_wrap<BinaryProxy<Scalar<T>, V, FUN>>>
-operator+(const T& lhs, const vec_wrap<V>& rhs) {
-    return vec_wrap<BinaryProxy<Scalar<T>, V, FUN>>(BinaryProxy<Scalar<T>, V, FUN>(vec_wrap<Scalar<T>>(lhs), rhs, FUN{}));
-}
-template <typename V, typename T, typename FUN = std::plus<get_type<V, Scalar<T>>>>
-EnableIf<rank<T>::value, vec_wrap<BinaryProxy<V, Scalar<T>, FUN>>>
-operator+(const vec_wrap<V>& lhs, const T& rhs) {
-    return vec_wrap<BinaryProxy<V, Scalar<T>, FUN>>(BinaryProxy<V, Scalar<T>, FUN>(lhs, vec_wrap<Scalar<T>>(rhs), FUN{}));
+// choose appropiate doit function base on operator's operands: lhs, rhs.
+template <typename FUN>
+struct RetType {
+    template <typename V1, typename V2>
+    static vec_wrap<BinaryProxy<V1, V2, FUN>> doit(const vec_wrap<V1>& lhs, const vec_wrap<V2>& rhs) {
+        return vec_wrap<BinaryProxy<V1, V2, FUN>>(BinaryProxy<V1, V2, FUN>(lhs, rhs, FUN{}));
+    }
+    
+    template <typename T, typename V>
+    static EnableIf<rank<T>::value!=0, vec_wrap<BinaryProxy<Scalar<T>, V, FUN>>> doit(const T& lhs, const vec_wrap<V>& rhs) {
+        return vec_wrap<BinaryProxy<Scalar<T>, V, FUN>>(BinaryProxy<Scalar<T>, V, FUN>(vec_wrap<Scalar<T>>(lhs), rhs, FUN{}));
+    }
+
+    template <typename V, typename T>
+    static EnableIf<rank<T>::value!=0, vec_wrap<BinaryProxy<V, Scalar<T>, FUN>>> doit(const vec_wrap<V>& lhs, const T& rhs) {
+        return vec_wrap<BinaryProxy<V, Scalar<T>, FUN>>(BinaryProxy<V, Scalar<T>, FUN>(lhs, vec_wrap<Scalar<T>>(rhs), FUN{}));
+    }
+};
+
+template <typename T1, typename T2, typename FUN = std::plus<typename get_type<T1, T2>::type>>
+auto operator+(const T1& lhs, const T2& rhs) -> decltype(RetType<FUN>::doit(lhs, rhs)) {
+    return RetType<FUN>::doit(lhs, rhs);
 }
 
-// Sub
-template <typename V1, typename V2, typename FUN = std::minus<get_type<V1, V2>>>
-vec_wrap<BinaryProxy<V1, V2, FUN>> operator-(const vec_wrap<V1>& lhs, const vec_wrap<V2>& rhs) {
-    return vec_wrap<BinaryProxy<V1, V2, FUN>>(BinaryProxy<V1, V2, FUN>(lhs, rhs, FUN{}));
-}
-template <typename T, typename V, typename FUN = std::minus<get_type<Scalar<T>, V>>>
-EnableIf<rank<T>::value, vec_wrap<BinaryProxy<Scalar<T>, V, FUN>>>
-operator-(const T& lhs, const vec_wrap<V>& rhs) {
-    return vec_wrap<BinaryProxy<Scalar<T>, V, FUN>>(BinaryProxy<Scalar<T>, V, FUN>(vec_wrap<Scalar<T>>(lhs), rhs, FUN{}));
-}
-template <typename V, typename T, typename FUN = std::minus<get_type<V, Scalar<T>>>>
-EnableIf<rank<T>::value, vec_wrap<BinaryProxy<V, Scalar<T>, FUN>>>
-operator-(const vec_wrap<V>& lhs, const T& rhs) {
-    return vec_wrap<BinaryProxy<V, Scalar<T>, FUN>>(BinaryProxy<V, Scalar<T>, FUN>(lhs, vec_wrap<Scalar<T>>(rhs), FUN{}));
+template <typename T1, typename T2, typename FUN = std::minus<typename get_type<T1, T2>::type>>
+auto operator-(const T1& lhs, const T2& rhs) -> decltype(RetType<FUN>::doit(lhs, rhs)) {
+    return RetType<FUN>::doit(lhs, rhs);
 }
 
-// Mul
-template <typename V1, typename V2, typename FUN = std::multiplies<get_type<V1, V2>>>
-vec_wrap<BinaryProxy<V1, V2, FUN>> operator*(const vec_wrap<V1>& lhs, const vec_wrap<V2>& rhs) {
-    return vec_wrap<BinaryProxy<V1, V2, FUN>>(BinaryProxy<V1, V2, FUN>(lhs, rhs, FUN{}));
-}
-template <typename T, typename V, typename FUN = std::multiplies<get_type<Scalar<T>, V>>>
-EnableIf<rank<T>::value, vec_wrap<BinaryProxy<Scalar<T>, V, FUN>>>
-operator*(const T& lhs, const vec_wrap<V>& rhs) {
-    return vec_wrap<BinaryProxy<Scalar<T>, V, FUN>>(BinaryProxy<Scalar<T>, V, FUN>(vec_wrap<Scalar<T>>(lhs), rhs, FUN{}));
-}
-template <typename V, typename T, typename FUN = std::multiplies<get_type<V, Scalar<T>>>>
-EnableIf<rank<T>::value, vec_wrap<BinaryProxy<V, Scalar<T>, FUN>>>
-operator*(const vec_wrap<V>& lhs, const T& rhs) {
-    return vec_wrap<BinaryProxy<V, Scalar<T>, FUN>>(BinaryProxy<V, Scalar<T>, FUN>(lhs, vec_wrap<Scalar<T>>(rhs), FUN{}));
+template <typename T1, typename T2, typename FUN = std::multiplies<typename get_type<T1, T2>::type>>
+auto operator*(const T1& lhs, const T2& rhs) -> decltype(RetType<FUN>::doit(lhs, rhs)) {
+    return RetType<FUN>::doit(lhs, rhs);
 }
 
-// Div
-template <typename V1, typename V2, typename FUN = std::divides<get_type<V1, V2>>>
-vec_wrap<BinaryProxy<V1, V2, FUN>> operator/(const vec_wrap<V1>& lhs, const vec_wrap<V2>& rhs) {
-    return vec_wrap<BinaryProxy<V1, V2, FUN>>(BinaryProxy<V1, V2, FUN>(lhs, rhs, FUN{}));
-}
-template <typename T, typename V, typename FUN = std::divides<get_type<Scalar<T>, V>>>
-EnableIf<rank<T>::value, vec_wrap<BinaryProxy<Scalar<T>, V, FUN>>>
-operator/(const T& lhs, const vec_wrap<V>& rhs) {
-    return vec_wrap<BinaryProxy<Scalar<T>, V, FUN>>(BinaryProxy<Scalar<T>, V, FUN>(vec_wrap<Scalar<T>>(lhs), rhs, FUN{}));
-}
-template <typename V, typename T, typename FUN = std::divides<get_type<V, Scalar<T>>>>
-EnableIf<rank<T>::value, vec_wrap<BinaryProxy<V, Scalar<T>, FUN>>>
-operator/(const vec_wrap<V>& lhs, const T& rhs) {
-    return vec_wrap<BinaryProxy<V, Scalar<T>, FUN>>(BinaryProxy<V, Scalar<T>, FUN>(lhs, vec_wrap<Scalar<T>>(rhs), FUN{}));
+template <typename T1, typename T2, typename FUN = std::divides<typename get_type<T1, T2>::type>>
+auto operator/(const T1& lhs, const T2& rhs) -> decltype(RetType<FUN>::doit(lhs, rhs)) {
+    return RetType<FUN>::doit(lhs, rhs);
 }
 
 // Output
